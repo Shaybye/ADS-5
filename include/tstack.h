4d133ed96f65e617e304ec9f647caab58ef642d9
Shaybye <159885796+Shaybye@users.noreply.github.com>
2024-04-14 17:00:30 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,28 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+    type arr[SIZE];
+    int top;
+  public:
+    TStack():top(-1){ }
+    type get() const {
+        return arr[top]
+    }
+    bool isEmpty() const {
+        return top == -1;
+    }
+    bool isFull() const {
+        return top == size - 1;
+    }
+    void pop() {
+        if (top >= 0)
+            top--;
+    }
+    void push(type item) {
+        if (top < size - 1)
+            arr[++top] == item;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
