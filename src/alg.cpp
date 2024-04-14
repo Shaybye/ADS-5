a1f0698f519bf5152165a668816831c1cd7709c6
Shaybye <159885796+Shaybye@users.noreply.github.com>
2024-04-14 21:37:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,13 +2,122 @@
 #include <string>
 #include <map>
 #include "tstack.h"
-
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+using namespace std;
+int getPriority(char symbol) {
+    switch (symbol) {
+    case '(':
+        return 0;
+        break;
+    case ')':
+        return 1;
+        break;
+    case '+':
+        return 2;
+        break;
+    case '-':
+        return 2;
+        break;
+    case '*':
+        return 3;
+        break;
+    case '/':
+        return 3;
+        break;
+    default:
+        break;
+    }
 }
-
+std::string infx2pstfx(std::string inf) {
+    TStack<char, 100> stack1;
+    string result = "";
+    for (int i = 0; i < inf.length(); i++) {
+        if (inf[i] >= '0' && inf[i] <= '9') {
+            if (inf[i + 1] >= '0' && inf[i + 1] <= '9' ) {
+                result += inf[i];
+            }
+            else {
+                result += inf[i];
+                result += ' ';
+            }
+        }
+        else {
+            if (inf[i] == '(')
+                stack1.push('(');
+            else if (inf[i] == ')') {
+                while (!stack1.isEmpty() && stack1.get() != '(') {
+                    result += stack1.get();
+                    result += ' ';
+                    stack1.pop();
+                }
+                if (!stack1.isEmpty())
+                    stack1.pop();
+            }
+            else {
+                if (getPriority(inf[i]) > getPriority(stack1.get())) {
+                    stack1.push(inf[i]);
+                }
+                else if (getPriority(inf[i]) <= getPriority(stack1.get())) {
+                    while (!stack1.isEmpty() && getPriority(inf[i]) <= getPriority(stack1.get())) {
+                        result += stack1.get();
+                        result += ' ';
+                        stack1.pop();
+                    }
+                    stack1.push(inf[i]);
+                }
+            }
+        }
+    }
+    while (!stack1.isEmpty()) {
+        result += stack1.get();
+        result += ' ';
+        stack1.pop();
+    }
+    return result;
+}
+int calculate(int a, int b, char symbol) {
+    switch (symbol) {
+    case '+':
+        return b + a;
+        break;
+    case '-':
+        return b - a;
+        break;
+    case '*':
+        return b * a;
+        break;
+    case '/':
+        return b / a;
+        break;
+    default:
+        break;
+    }
+}
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+    TStack<int, 100> stack2;
+    int result = 0;
+    string nums="";
+    for (int i = 0; i < pref.length(); i++) {
+        if (pref[i] >= '0' && pref[i] <= '9') {
+            if (pref[i + 1] >= '0' && pref[i + 1] <= '9') {
+                nums += pref[i];
+            }
+            else {
+                nums += pref[i];
+                stack2.push(stoi(nums));
+                nums = "";
+            }
+        }
+        else if (pref[i] == ' ')
+            continue;
+        else {
+            int a = stack2.get();
+            stack2.pop();
+            int b = stack2.get();
+            stack2.pop();
+            stack2.push(calculate(a, b, pref[i]));
+        }
+    }
+    int res = stack2.get();
+    stack2.pop();
+    return res;
 }
